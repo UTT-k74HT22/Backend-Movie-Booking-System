b9366af436086debfe8c8ea9519ac77607e0221a
DungDev <hoangdinhdung0205@gmail.com>
2025-10-11 23:33:25 +0700

    fix: chỉnh lại flow auth

--- src/main/java/com/trainning/movie_booking_system/dto/request/test.java
+++
@@ -1,4 +0,0 @@
-package com.trainning.movie_booking_system.dto.request;
-
-public class test {
-}
--- src/main/java/com/trainning/movie_booking_system/untils/constraint/test.java
+++
@@ -1,4 +0,0 @@
-package com.trainning.movie_booking_system.untils.constraint;
-
-public class test {
-}
