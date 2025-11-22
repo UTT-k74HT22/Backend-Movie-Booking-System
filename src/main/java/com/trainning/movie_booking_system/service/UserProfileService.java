5f1752d52072f3268f893caf316dd4264c2920da
Dohoan-2511 <dohoan25112005@gmail.com>
2025-11-22 22:33:16 +0700

    feat(Profile user):Base thư mục user

---
+++ src/main/java/com/trainning/movie_booking_system/service/UserProfileService.java
@@ -0,0 +1,4 @@
+package com.trainning.movie_booking_system.service;
+
+public class UserProfileService {
+}
---
+++ src/main/java/com/trainning/movie_booking_system/service/impl/UserProfileServiceImpl.java
@@ -0,0 +1,4 @@
+package com.trainning.movie_booking_system.service.impl;
+
+public class UserProfileServiceImpl {
+}
